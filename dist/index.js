9bd3a0645a2c97cc19221c419da1cf3204ea4234
Sahil.Agarwal <sahil.agarwal@SahilAgarwals-MacBook-Pro.local>
2024-01-04 16:46:33 +0530

    typescript Function Type

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let functioncheck;
+function add(input1, input2) {
+    return input1 + input2;
+}
+// this line throws error
+// functioncheck = add
---
+++ index.ts
@@ -0,0 +1,9 @@
+let functioncheck: (param1: string, param2: string) => string
+
+
+function add(input1: string, input2: number): string {
+    return input1 + input2;
+}
+
+ // this line throws error
+// functioncheck = add
